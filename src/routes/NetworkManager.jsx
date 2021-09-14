77e1052fc03781cc342dffca55dc63220104c2a9
MrJackdaw <MrJackdaw@users.noreply.github.com>
2021-09-13 18:30:06 -0700

    * updaet package * absolutely breaks network-manager demo

--- src/routes/NetworkManager.jsx
+++ src/routes/NetworkManager.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Network from "../network";
+import "./NetworkManager.scss";
 
 const NetworkManager = () => {
-  return <p>Yes hello I am in progress</p>;
+  const fetchItems =  async () => {
+  const bus =  await Network.recentlyRegisteredOregonBusinesses()
+  console.log(bus)
+  }
+  return (
+    <section>
+      <h4>Network Manager Code</h4>
+
+      <pre>
+        <code>/** </code>
+        <code>* `NetworkLayer` has two properties: </code>
+        <code>
+          * -> `METHODS`: a string-string dictionary of request types (GET,
+          POST, DEL, etc)
+        </code>
+        <code>
+          * -> `APIConfig`: the object that uses your predefined endpoints
+          ("routes") to make requests{" "}
+        </code>
+        <code>* </code>
+        <code>
+          * Export an instance of `APIConfig` that looks like the one below:{" "}
+        </code>
+        <code>
+          * view a "service" file in this * directory to see it in action
+        </code>
+        <code>*/ </code>
+        <code>import NetworkLayer from '@jackcom/app-network-layer';</code>
+        <code>import Routes from './endpoints'; </code>
+        <code>const Network = new APIConfig(Routes); export default Network;</code>
+      </pre>
+
+      <button>Fetch latest Business registrations</button>
+    </section>
+  );
 };
 
 export default NetworkManager;
